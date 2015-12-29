70e0b0c58dd1de3fa2b736fb7e6e83d34b5103ee
Hikmat Dhamee <mehikmat@users.noreply.github.com>
2015-12-29 12:18:56 +0545

    Update generate_so_file.sh

--- Data_Structures_And_Algorithms/Chapter1_Java_Fundamentals/native_keyword/bin/generate_so_file.sh
+++ Data_Structures_And_Algorithms/Chapter1_Java_Fundamentals/native_keyword/bin/generate_so_file.sh
@@ -12,7 +12,7 @@ cd jni
 #gcc -c -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/win32" -shared -o hello.so HelloJNI.c // for windows
 
 #// Compile-only with -c flag. Output is HElloJNI.o
-gcc -c -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -fPIC -c HelloJNI.c
+gcc -c -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -fPIC HelloJNI.c
 #// Link into shared library "HelloJNI.so"
 gcc -shared -Wl,-soname,libhellojni.so -o libhellojni.so HelloJNI.o
 
