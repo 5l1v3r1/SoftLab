c12e7f069d98fa3a89413d711b2dfb1512550550
Hikmat <me.hemant.available@gmail.com>
2016-04-19 14:08:37 +0545

    adding nn

--- Machine_Learning/NN/LogicFunctions/src/main/java/ml/nn/AND.java
+++ Machine_Learning/NN/LogicFunctions/src/main/java/ml/nn/AND.java
@@ -1,89 +1,94 @@
 package ml.nn;
 
 /**
+ * Using Hebbian Learning, the AND logic function simulation
+ *
  * Created by hdhamee on 4/18/16.
  */
 public class AND {
 
     public static void main(String a[]){
-        int training[][] = new int[][]{
+        //prepare training data
+        int trainingData[][] = new int[][]{
+                //bias,input1,input2,output
                 {1,1,1,1},
-                {1,1,0,0},
-                {1,0,1,0},
-                {1,0,0,0}};
+                {1,1,-1,-1},
+                {1,-1,1,-1},
+                {1,-1,-1,-1}};
 
-        NeuralNetwork net = new NeuralNetwork(training);
-        net.training();
+        // create neural net
+        NeuralNetwork net = new NeuralNetwork(trainingData);
 
-        net.testing(new int[]{1,1,1,0});
-        net.testing(new int[]{1,0,0,0});
+        //train the net
+        net.train();
+
+        //test trained net
+        net.test(new int[]{1, 1, 1, 0});
+        net.test(new int[]{1, 1, -1, 0});
+        net.test(new int[]{1, -1, 1, 0});
+
+        //save net
+        System.out.println("Serialize net");
+
+        //use it for other inputs
+        System.out.println("Write code to read net and use it elsewhere");
     }
 
 
     public static class NeuralNetwork {
-        Neuron neurons[] = new Neuron[3];
+        Neuron neuron = new Neuron();
         int training[][] = new int[4][];
-        double alpha = 0.1;
 
         public NeuralNetwork(int trainingdata[][]) {
             training = trainingdata;
-            neurons = new Neuron[3];
-            for (int j = 0; j < neurons.length; j++) {
-                neurons[j] = new Neuron();
-            }
+            neuron = new Neuron();
         }
 
-        public void training() {
-            int k = 1;
-
+        public void train() {
+            System.out.println("[initially] " + "b=" + neuron.getWeights()[0] + " w1=" + neuron.getWeights()[1] + " w2=" + neuron.getWeights()[2]);
             for (int i = 0; i < training.length; i++) {
-                System.out.println("["+i+"]th round " + "w1=" + neurons[1].getWeight() + " w2=" + neurons[2].getWeight() );
+                //Update all the neuron weight
+                neuron.updateWeights(training[i]);
 
-                int inputs[] = training[i];
-                double output = getNeuralNetOutput(neurons, inputs);
-
-                //Update all the neuron's weight
-                for (int j = 0; j < neurons.length; j++) {
-                    neurons[j].updateWeight(inputs[inputs.length - 1] * inputs[j] * alpha);
-                }
-
+                System.out.println("[" + i + "]th round " + "b=" + neuron.getWeights()[0] + " w1=" + neuron.getWeights()[1] + " w2=" + neuron.getWeights()[2]);
             }
         }
 
-        public double getNeuralNetOutput(Neuron[] neurons, int inputs[]) {
+        public double getNeuralNetOutput(Neuron neuron, int inputs[]) {
             //get all the outputs and add them together
-            double output = 0;
-            for (int j = 0; j < neurons.length; j++) {
-                output += neurons[j].getOutput(inputs[j]);
-            }
-            return output;
+            return neuron.getWeightedSum(inputs);
         }
 
-        public void testing(int[] inputs) {
-            System.out.println(getNeuralNetOutput(neurons, inputs));
+        public void test(int[] inputs) {
+            System.out.println(getNeuralNetOutput(neuron, inputs) > 0 ? 1:-1);
         }
     }
 
 
     // Neuron
     public static class Neuron {
-        double weight;
+        double[] weights;
 
         public Neuron(){
-            weight=0;
+            weights = new double[3];
         }
 
-        public double getOutput(int x){
-            return x*weight;
+        public double getWeightedSum(int[] inputs){
+            int output = 0;
+            for (int j = 0; j < inputs.length - 1; j++) {
+                output += (inputs[j] * weights[j]);
+            }
+            return output;
         }
 
-        public void updateWeight(double update){
-            weight+=update;
+        public void updateWeights(int[] inputs){
+            for (int j = 0; j < inputs.length-1; j++) {
+                weights[j] += (inputs[inputs.length-1] * inputs[j]);
+            }
         }
 
-        public double getWeight() {
-            return weight;
+        public double[] getWeights(){
+            return weights;
         }
     }
-
 }
