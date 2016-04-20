373b4cc231bb3903a142f42d69a7d795267fcd14
Hikmat <me.hemant.available@gmail.com>
2016-04-20 18:33:50 +0545

    adding nn

---
+++ Machine_Learning/Neuroph/NeurophTour/src/main/java/ml/imagerecognition/ImageRecognition.java
@@ -0,0 +1,11 @@
+package ml.imagerecognition;
+
+/**
+ *
+ * http://neuroph.sourceforge.net/image_recognition.html
+ *
+ *
+ * Created by hdhamee on 4/20/16.
+ */
+public class ImageRecognition {
+}
