ccc813704241ecde92a5c0d6488df07d042dcb12
Hikmat <me.hemant.available@gmail.com>
2016-07-07 12:12:59 +0545

    adding tez

--- Tools/Hadoop/Tez/cascading-tez/src/main/java/tezdemo/Main.java
+++ Tools/Hadoop/Tez/cascading-tez/src/main/java/tezdemo/Main.java
@@ -1,7 +1,11 @@
 package tezdemo;
 
 import cascading.flow.Flow;
+import cascading.flow.FlowRuntimeProps;
 import cascading.flow.tez.Hadoop2TezFlowConnector;
+import cascading.operation.Identity;
+import cascading.pipe.Each;
+import cascading.pipe.GroupBy;
 import cascading.pipe.Pipe;
 import cascading.property.AppProps;
 import cascading.scheme.hadoop.TextDelimited;
@@ -16,18 +20,24 @@ import java.util.Properties;
  * Created by hdhamee on 5/16/16.
  */
 public class Main {
+
     public void es(){
         Tap src1 = new Hfs(new TextDelimited(new Fields("a","b","c","d","e","f"),";"), "input/input.txt", SinkMode.KEEP);
         Tap snk1 = new Hfs(new TextDelimited(new Fields("a"),";"), "output.txt", SinkMode.REPLACE);
 
         Pipe pipe1 = new Pipe("copy1");
+        pipe1 = new Each(pipe1,new Identity());
+        pipe1 = new GroupBy(pipe1,new Fields("a"));
 
-        //set tez libs path
-        Properties properties = new Properties();
-        properties.setProperty("tez.lib.uris","/apps/tez-0.8.4-SNAPSHOT/tez-0.8.4-SNAPSHOT.tar.gz");
-        Flow flow2 = new Hadoop2TezFlowConnector(properties).connect(src1,snk1,pipe1);
+
+        Properties properties = FlowRuntimeProps.flowRuntimeProps()
+                .setGatherPartitions(4)
+                .buildProperties();
 
         AppProps.setApplicationJarClass(properties,Main.class);
+        AppProps.setApplicationName(properties,"Tez-Test");
+
+        Flow flow2 = new Hadoop2TezFlowConnector(properties).connect(src1,snk1,pipe1);
 
         flow2.complete();
     }
