3a7f3742ee86d87686536a7cd9ecd79be3cb6a3c
Hikmat <me.hemant.available@gmail.com>
2016-11-21 11:50:00 +0545

    adding spark sql

---
+++ Tools/BigData/Spark/spark-sql/src/main/scala/demo/stream/SparkSQLMain.scala
@@ -0,0 +1,39 @@
+package demo.stream
+
+import java.text.SimpleDateFormat
+
+import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by hdhamee on 11/15/16.
+ */
+object SparkSQLMain {
+  val dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+  def main(args: Array[String]) {
+    // table schema
+    val schemaString = "name age degree"
+    val schema = StructType(schemaString.split(" ").map(fieldName => StructField(fieldName,StringType,true)))
+
+    // spark context
+    val appName = "Spark SQL Interactive data processing"
+    val conf = new SparkConf().setAppName(appName).setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    // sql context
+    val sqlContext = new SQLContext(sc)
+
+    //data source
+    val rawRdd = sc.textFile("data/data.txt").map(line => line.split(" ",-1)).map(a => Row(a(0),a(1),a(2)))
+
+    // sql operation
+    val df = sqlContext.createDataFrame(rawRdd,schema)
+    val a =  df.select("name","age","degree").filter(df("age") > 30).show()
+
+    // stop driver program
+    sc.stop()
+
+  }
+}
