3838f11013278f9ca5f3d7d07157c0d7e5a3823f
Hikmat Dhamee <mehikmat@users.noreply.github.com>
2017-02-28 14:58:56 +0545

    Update SimpleWebCrawler.java

--- WebCrawler/src/main/java/SimpleWebCrawler.java
+++ WebCrawler/src/main/java/SimpleWebCrawler.java
@@ -16,6 +16,7 @@ public class SimpleWebCrawler {
     public static final String VALID_URL = "^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$";
     public static List<List<String>> crawledData = new ArrayList<List<String>>();
 
+    //1. Pick a URL from the frontier, Here the seedUrl
     public SimpleWebCrawler(String seedUrl, int maxVisitCount) throws Exception {
         if(seedUrl.matches(VALID_URL)){
             System.out.println("Malformed seed url! " + seedUrl);
@@ -30,17 +31,18 @@ public class SimpleWebCrawler {
         while (currentVisitCount < maxVisitCount && !urls.isEmpty()) {
             String url = urls.remove(0);
 
+            //4. Check if you have already visited the URL
             if (url.matches(VALID_URL) && !visitedUrls.contains(url)){
                 visitedUrls.add(url);
 
+                 //2. Fetch the HTML code
                 Document doc = Jsoup.connect(url).get();
 
                 currentVisitCount++;
 
                 String text = doc.text();
 
-                // Skip any pages where text on doc is identical to existing
-                // doc (e.g., http://example.com and http://exaple.com/index.html
+               //4. Check if you have already crawled the URLs
                 boolean process = true;
                 for (List<String> ls : crawledData) {
                     if (text.equals(ls.get(1))) {
@@ -49,11 +51,14 @@ public class SimpleWebCrawler {
                     }
                 }
 
+                //4. if not then add it to index
                 if (process) {
+                    //3. Parse the HTML to extract links to other URLs
                     Elements inPageLinks = doc.select("a[href]");
 
                     for (Element element :inPageLinks) {
                         String link = element.attr("abs:href");
+                        //5. For each link add it to url list to be again visited at Step 4
                         urls.add(link);
                     }
 
